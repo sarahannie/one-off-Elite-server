2c53b82f25ec1da66c42da820e1e57e928b78d4c
sarahannie <sarahndianekwu@gmail.com>
2024-08-18 12:47:35 +0100

    one-off elite server

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configure Nodemailer with environment variables
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Replace with your email service
+    auth: {
+        user: process.env.EMAIL_USER, // Use the email user from the .env file
+        pass: process.env.EMAIL_PASS  // Use the email password from the .env file
+    }
+});
+
+app.post('/send-email', (req, res) => {
+    const { gname, gmail, phone, service, message } = req.body;
+
+    const mailOptions = {
+        from: gmail,
+        to: 'sarahndianekwu@gmail.com', 
+        subject: 'New Appointment Request',
+        text: `Name: ${gname}\n Email: ${gmail}\n Phone: ${phone}\nService: ${service}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send(`Appointment has been Succesfully Booking😍\n we will get in touch soon😊`);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
